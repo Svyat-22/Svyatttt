754dd5438fffa8ba704b4040af00857a2ba439ae
Svyat-22 <vane4ka572@gmail.com>
2019-09-24 13:36:25 +0300

    first

---
+++ Svyatttt/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+ char name[50];
+ cout<<"what is your name?";
+ cin>>name;
+ cout<<"privet "
+ <<name<<endl; 
+ char age[50];
+ cout<<"skolko let?";
+ cin>>age;
+ cout<<"tebe "
+ <<age<<endl;
+return 0;
+}
+
